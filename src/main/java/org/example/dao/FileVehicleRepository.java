e3a76a71536fef24ef690c712b74f5fa0544e6da
fenyvesvolgyimate <fenyves96@gmail.com>
2023-01-20 14:57:30 +0100

    implement VehicleRepository

---
+++ src/main/java/org/example/dao/FileVehicleRepository.java
@@ -0,0 +1,76 @@
+package org.example.dao;
+
+import dao.VehicleRepository;
+import entity.Vehicle;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileVehicleRepository implements VehicleRepository {
+
+    public FileVehicleRepository(){
+        clearFile();
+    }
+
+    private void clearFile() {
+        FileWriter fileWriter;
+        try {
+            fileWriter = new FileWriter("vehicles.txt");
+            fileWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    final String SEPARATOR = ";";
+
+    @Override
+    public void saveVehicle(Vehicle vehicle) {
+        FileWriter fileWriter;
+        try {
+            fileWriter = new FileWriter("vehicles.txt", true);
+            fileWriter.write(generateVehicleRow(vehicle) + System.lineSeparator());
+            fileWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String generateVehicleRow(Vehicle vehicle) {
+        return vehicle.getRegistrationNumber() + SEPARATOR +
+                vehicle.getMake() + SEPARATOR +
+                vehicle.getModel() + SEPARATOR +
+                vehicle.getNumberOfSeats() + SEPARATOR +
+                vehicle.getVehicleType();
+    }
+
+    @Override
+    public Vehicle findVehicleByRegistrationNumber(String registrationNumber) {
+        String line;
+        Vehicle result = null;
+        try {
+            BufferedReader bufferreader = new BufferedReader(new FileReader("vehicles.txt"));
+            while((line = bufferreader.readLine()) != null){
+                Vehicle vehicle = parseVehicle(line);
+                if(vehicle.getRegistrationNumber().equals(registrationNumber))
+                    result = vehicle;
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return result;
+    }
+
+    private Vehicle parseVehicle(String line) {
+        String [] vehicleAttributes = line.split(SEPARATOR);
+        Vehicle vehicle = new Vehicle();
+        vehicle.setRegistrationNumber(vehicleAttributes[0]);
+        vehicle.setMake(vehicleAttributes[1]);
+        vehicle.setModel(vehicleAttributes[2]);
+        vehicle.setNumberOfSeats(Integer.parseInt(vehicleAttributes[3]));
+        vehicle.setVehicleType(vehicleAttributes[4]);
+        return vehicle;
+    }
+}
---
+++ src/test/java/FileVehicleRepositoryTests.java
@@ -0,0 +1,69 @@
+import dao.VehicleRepository;
+import entity.Vehicle;
+import org.example.dao.FileVehicleRepository;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FileVehicleRepositoryTests {
+    @Test
+    public void testSaveVehicle(){
+        VehicleRepository repository = new FileVehicleRepository();
+        Vehicle opel = createOpelAstra();
+        repository.saveVehicle(opel);
+        assertEquals(getLineFromFile(0), "AA:BC-123;Opel;Astra;5;m1");
+
+        Vehicle volkswagen = createVolswagen();
+        repository.saveVehicle(volkswagen);
+        assertEquals(getLineFromFile(1), "BB:BC-123;Volkswagen;Golf;5;n1");
+    }
+
+    private static Vehicle createVolswagen() {
+        Vehicle volkswagen = new Vehicle();
+        volkswagen.setMake("Volkswagen");
+        volkswagen.setModel("Golf");
+        volkswagen.setNumberOfSeats(5);
+        volkswagen.setRegistrationNumber("BB:BC-123");
+        volkswagen.setVehicleType("n1");
+        return volkswagen;
+    }
+
+    private static Vehicle createOpelAstra() {
+        Vehicle vehicle = new Vehicle();
+        vehicle.setMake("Opel");
+        vehicle.setModel("Astra");
+        vehicle.setNumberOfSeats(5);
+        vehicle.setRegistrationNumber("AA:BC-123");
+        vehicle.setVehicleType("m1");
+        return vehicle;
+    }
+
+    @Test
+    public void testFindVehicleByRegistrationNumber(){
+        FileVehicleRepository repository = new FileVehicleRepository();
+        Vehicle vehicle = createOpelAstra();
+        repository.saveVehicle(createOpelAstra());
+        Vehicle result = repository.findVehicleByRegistrationNumber(vehicle.getRegistrationNumber());
+        assertEquals(vehicle.getModel(), result.getModel());
+    }
+
+    private String getLineFromFile(int lineNumber){
+        String line = null;
+        try {
+            BufferedReader bufferreader = new BufferedReader(new FileReader("vehicles.txt"));
+            int currentLineNumber = 0;
+            while(lineNumber != currentLineNumber){
+                line = bufferreader.readLine();
+                currentLineNumber++;
+            }
+            line = bufferreader.readLine();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return line;
+    }
+}
